3ddb6be3e702874452785fe8ef4287bb67ab37d2
luongvuc0622i1 <vuhienluong.td151218@gmail.com>
2022-12-05 17:06:27 +0700

    init

---
+++ src/main/java/com/QuarQuizzApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuarQuizzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuarQuizzApplication.class, args);
+	}
+
+}
